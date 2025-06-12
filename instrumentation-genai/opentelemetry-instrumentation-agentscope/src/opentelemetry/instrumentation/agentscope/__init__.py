3df2ee196b5a7faa100576d71554dcd913c46b9a
qipeng <qp467389@alibaba-inc.com>
2025-06-12 11:00:37 +0800

    fix: Fix spell mistake.

--- instrumentation-genai/opentelemetry-instrumentation-agentscope/src/opentelemetry/instrumentation/agentscope/__init__.py
+++ instrumentation-genai/opentelemetry-instrumentation-agentscope/src/opentelemetry/instrumentation/agentscope/__init__.py
@@ -1,14 +1,16 @@
 from typing import Any, Collection
 from wrapt import wrap_function_wrapper
+from opentelemetry.instrumentation.utils import unwrap
 from opentelemetry.instrumentation.agentscope.package import _instruments
 from opentelemetry.instrumentation.agentscope._wrapper import AgentscopeRequestWrapper, AgentscopeToolcallWrapper
 from opentelemetry import trace as trace_api
-from opentelemetry.instrumentation.instrumentor import BaseInstrumentor  # type: ignore
+from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
 from opentelemetry.instrumentation.version import (
     __version__,
 )
 
-"""OpenTelemetry exporters for BlackSheep instrumentation"""
+"""OpenTelemetry exporters for agentscope instrumentation https://github.com/modelscope/agentscope"""
+
 _MODULE = "agentscope.models.model"
 _TOOLKIT = "agentscope.service.service_toolkit"
 __all__ = ["AgentScopeInstrumentor"]
@@ -36,13 +38,8 @@ class AgentScopeInstrumentor(BaseInstrumentor):  # type: ignore
             wrapper=AgentscopeToolcallWrapper(tracer=tracer),
         )
     def _uninstrument(self, **kwargs: Any) -> None:
-        wrap_function_wrapper(
-            module=_MODULE,
-            name="ModelWrapperBase.__init__",
-            wrapper=None,
-        )
-        wrap_function_wrapper(
-            module=_TOOLKIT,
-            name="ServiceToolkit._execute_func",
-            wrapper=None,
-        )
+        
+        import agentscope.models.model
+        unwrap(agentscope.models.model.ModelWrapperBase, "__init__")
+        import agentscope.service.service_toolkit
+        unwrap(agentscope.service.service_toolkit.ServiceToolkit, "_execute_func")
