f240663a3c963f4c5819b476f32bc7f8cc547316
minimAluminiumalism <caixuesen@outlook.com>
2025-12-02 12:09:33 +0800

    fix: correct timestamp calculation in dify instrumentation

--- instrumentation-loongsuite/loongsuite-instrumentation-dify/src/opentelemetry/instrumentation/dify/utils.py
+++ instrumentation-loongsuite/loongsuite-instrumentation-dify/src/opentelemetry/instrumentation/dify/utils.py
@@ -5,11 +5,7 @@ from datetime import datetime
 def get_timestamp_from_datetime_attr(obj, attr_name):
     attr_value = getattr(obj, attr_name, None)
     if attr_value is not None and isinstance(attr_value, datetime):
-        timestamp = (
-            attr_value.timestamp() * 1_000_000_000
-            + attr_value.microsecond * 1_000
-        )
-        return int(timestamp)
+        return int(attr_value.timestamp() * 1_000_000_000)
     return time.time_ns()
 
 
